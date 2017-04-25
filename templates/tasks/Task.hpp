15ecd9c659317a2610d8fed48d2db9a01cc773f1
Ricardo Sanchez <ricardosan@uma.es>
2017-04-25 17:07:32 +0200

    build

--- templates/tasks/Task.hpp
+++ templates/tasks/Task.hpp
@@ -5,9 +5,9 @@
 
 #include "simulation_vrep/TaskBase.hpp"
 
-namespace simulation_vrep {
+namespace simulation_vrep{
 
-    /*! \class Task 
+    /*! \class Task
      * \brief The task context provides and requires services. It uses an ExecutionEngine to perform its functions.
      * Essential interfaces are operations, data flow ports and properties. These interfaces have been defined using the oroGen specification.
      * In order to modify the interfaces you should (re)use oroGen and rely on the associated workflow.
@@ -19,7 +19,7 @@ namespace simulation_vrep {
          task('custom_task_name','simulation_vrep::Task')
      end
      \endverbatim
-     *  It can be dynamically adapted when the deployment is called with a prefix argument. 
+     *  It can be dynamically adapted when the deployment is called with a prefix argument.
      */
     class Task : public TaskBase
     {
@@ -35,9 +35,9 @@ namespace simulation_vrep {
          */
         Task(std::string const& name = "simulation_vrep::Task");
 
-        /** TaskContext constructor for Task 
-         * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices. 
-         * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task. 
+        /** TaskContext constructor for Task
+         * \param name Name of the task. This name needs to be unique to make it identifiable for nameservices.
+         * \param engine The RTT Execution engine to be used for this task, which serialises the execution of all commands, programs, state machines and incoming events for a task.
          * 
          */
         Task(std::string const& name, RTT::ExecutionEngine* engine);
@@ -75,7 +75,7 @@ namespace simulation_vrep {
          *
          * The error(), exception() and fatal() calls, when called in this hook,
          * allow to get into the associated RunTimeError, Exception and
-         * FatalError states. 
+         * FatalError states.
          *
          * In the first case, updateHook() is still called, and recover() allows
          * you to go back into the Running state.  In the second case, the
